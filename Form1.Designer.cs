0302c628a917ba1775fdbb6e1e6c6ec48521cfac
Churc <Churc@DESKTOP-GE73V8E>
2022-03-10 16:35:52 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PaperScissorsRockApp
+{
+    partial class ViewPort
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewPort));
+            this.TaskBarBG = new System.Windows.Forms.PictureBox();
+            this.ExitButton = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.TaskBarBG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ExitButton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TaskBarBG
+            // 
+            this.TaskBarBG.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.TaskBarBG.Location = new System.Drawing.Point(0, 0);
+            this.TaskBarBG.MinimumSize = new System.Drawing.Size(70, 30);
+            this.TaskBarBG.Name = "TaskBarBG";
+            this.TaskBarBG.Size = new System.Drawing.Size(1212, 30);
+            this.TaskBarBG.TabIndex = 0;
+            this.TaskBarBG.TabStop = false;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Image = ((System.Drawing.Image)(resources.GetObject("ExitButton.Image")));
+            this.ExitButton.Location = new System.Drawing.Point(749, 0);
+            this.ExitButton.MaximumSize = new System.Drawing.Size(25, 25);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(25, 25);
+            this.ExitButton.TabIndex = 1;
+            this.ExitButton.TabStop = false;
+            // 
+            // ViewPort
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.TaskBarBG);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "ViewPort";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.TaskBarBG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ExitButton)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox TaskBarBG;
+        private System.Windows.Forms.PictureBox ExitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaperScissorsRockApp
+{
+    public partial class ViewPort : Form
+    {
+        public ViewPort()
+        {
+            InitializeComponent();
+            this.TaskBarBG.Width = this.Width;
+            this.TaskBarBG.Height = 30;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.TaskBarBG.Height = 30;
+            this.TaskBarBG.Width = this.Width;
+        }
+    }
+}
